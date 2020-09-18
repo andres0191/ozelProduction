cba713c1ed42d92f1dbc26b50b182cfd6e411efc
andres0191 <andres_felipe0191@hotmail.com>
2020-09-17 21:19:56 -0500

    update home

--- ozelproductions/src/App.js
+++ ozelproductions/src/App.js
@@ -9,22 +9,16 @@ import logo from './components/images/icons/logotomorrowland.png'
 export default function App() {
   return (
     <Router>
-      <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light header" >
-        <img src="https://ozelproducciones.com/app/themes/ozel/dist/images/iso-triangle-white.svg?id=1cb39a9af6741f0793b3" width="50"/>
-        <a class="navbar-brand" href="/"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <Link className='nav-item nav-link' style={{color: 'white'}} to='/'>Inicio<span class="sr-only">(current)</span></Link>
-            <Link className='nav-item nav-link' style={{color: 'white'}} to='/About'>Nosotros<span class="sr-only">(current)</span></Link>
-            <Link className='nav-item nav-link' style={{color: 'white'}} to='/Login'>Boleteria<span class="sr-only">(current)</span></Link>
-          </div>
-        </div>
-        </nav>
-      </header>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light header" >
+            <img src="https://ozelproducciones.com/app/themes/ozel/dist/images/iso-triangle-white.svg?id=1cb39a9af6741f0793b3" width="50"/>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <Link className='nav-item nav-link' style={{color: 'white'}} to='/'>Inicio<span class="sr-only">(current)</span></Link>
+                <Link className='nav-item nav-link' style={{color: 'white'}} to='/About'>Nosotros<span class="sr-only">(current)</span></Link>
+                <Link className='nav-item nav-link' style={{color: 'white'}} to='/Login'>Boleteria<span class="sr-only">(current)</span></Link>
+              </div>
+            </div>
+          </nav>
         <Switch>
           <Route path="/About">
             <About />
@@ -39,7 +33,3 @@ export default function App() {
     </Router>
   );
 }
-
-/* <a class="nav-item nav-link active " className='lettercolor' href="/">Inicio <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link " className='lettercolor' href="/About">Nosotros</a>
-            <a class="nav-item nav-link " className='lettercolor' href="/Login">Registro</a> */
